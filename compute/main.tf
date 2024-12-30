a9036f158cea1fed7ef00000e08a728ec498ec95
narendraba <ballipallinarendra@gmail.com>
2024-12-30 11:22:17 +0530

    Create main.tf

---
+++ compute/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket         = "demo-workspace"
+    key            = "compute/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "my-table"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "compute" {
+  ami           = "ami-053b12d3152c0cc71"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "compute-instance"
+  }
+}
