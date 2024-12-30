68dd8c550510af8988961a46a31db942c2d4bd85
narendraba <ballipallinarendra@gmail.com>
2024-12-30 18:52:47 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+  default     = "ami-053b12d3152c0cc71"
+}
