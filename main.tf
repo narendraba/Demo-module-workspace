0bfb33ec2224763f2da3c2c810a6571c4d4f3b68
narendraba <ballipallinarendra@gmail.com>
2025-01-21 16:25:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket         = "demo-workspace"
-    key            = "demo-module-workspace5/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "my-table"
-  }
-}
+
 
 provider "aws" {
   region = "ap-south-1"
