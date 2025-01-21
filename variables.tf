c13237cda7753f9e6b2ecf2c57511b8a48d40b00
narendraba <ballipallinarendra@gmail.com>
2025-01-21 14:04:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "region" {
   default     = "ap-south-1"
 }
 
-variable "instance_type" {
-  description = "The type of instance to use"
-  type        = string
+#variable "instance_type" {
+#  description = "The type of instance to use"
+#  type        = string
   #default     = "t2.micro"
-}
+#}
 
 variable "ami" {
   description = "The AMI to use for the instance"
