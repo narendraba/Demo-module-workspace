7c77dd0a5242133f2f79e85bb0e830f34546a270
narendraba <ballipallinarendra@gmail.com>
2025-01-21 14:42:47 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -3,20 +3,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-#resource "aws_instance" "compute" {
-#  ami           = var.ami
-#  instance_type = var.instance_type
-#  tags = {
-#    Name = "compute-instance"
-#  }
-#}
-
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c7217cdde317cfec"  # Replace with your desired AMI ID
+resource "aws_instance" "compute" {
+  ami           = var.ami
   instance_type = var.instance_type
-
   tags = {
-    Name = "ExampleInstance"
+    Name = "compute-instance"
   }
 }
+
