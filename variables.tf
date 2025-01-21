7a34ba5cabb36724d4893a50c8d07170131a040b
narendraba <ballipallinarendra@gmail.com>
2025-01-21 13:38:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "instance_type" {
   description = "The type of instance to use"
   type        = string
-  default     = "t2.micro"
+  #default     = "t2.micro"
 }
 
 variable "ami" {
